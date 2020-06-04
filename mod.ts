98210b1f05849336d28c1b5de84af65dd08a4917
Daniel Beeke <daniel.beeke@gmail.com>
2020-06-04 18:33:57 +0200

    It must start with a letter and later on may use numbers.

--- mod.ts
+++ mod.ts
@@ -58,7 +58,7 @@ function parseFile(filepath: string) {
 }
 
 function isVariableStart(str: string): boolean {
-  return /^[a-zA-Z0-9_ ]*=/.test(str);
+  return /^[a-zA-Z_ ]([a-zA-Z0-9_ ])*=/.test(str);
 }
 
 function cleanQuotes(value: string = ""): string {
